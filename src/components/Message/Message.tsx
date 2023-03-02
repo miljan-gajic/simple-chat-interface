6dfbd53a853fe6ce4dc1dca8fca49519d1a44816
Miljan Gajic <miljan.gajic@bsdex.de>
2023-03-02 09:43:52 +0100

    removing boilerplate code from vite bootrstrap app and setting up folder structure

---
+++ src/components/Message/Message.tsx
@@ -0,0 +1,5 @@
+const Message: React.FC = () => {
+  return <div>Message</div>;
+};
+
+export default Message;
---
+++ src/components/MessagesList/MessagesList.tsx
@@ -0,0 +1,5 @@
+const MessagesList: React.FC = () => {
+  return <div>Hello from Messages List</div>;
+};
+
+export default MessagesList;
